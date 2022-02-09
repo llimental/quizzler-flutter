711899677412f8c427d0578ab59fd253b6f9dcac
ideal_lust3r <s1skyblue1s@gmail.com>
2022-02-10 02:42:59 +0900

    update
    some..

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ideal_lust3r/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ideal_lust3r/StudioProjects/quizzler-flutter"
+export "FLUTTER_ROOT=/Users/ideal_lust3r/Documents/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ideal_lust3r/StudioProject/quizzler-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/ideal_lust3r/StudioProjects/quizzler-flutter/lib/main.dart"
+export "FLUTTER_TARGET=/Users/ideal_lust3r/StudioProject/quizzler-flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVF
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/ideal_lust3r/StudioProjects/quizzler-flutter/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/ideal_lust3r/StudioProject/quizzler-flutter/.dart_tool/package_config.json"
